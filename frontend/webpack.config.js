43c55234c677ff326c14dd9389c61a9fcddf1f57
VincentWang1024 <v1ncent.wang1024@gmail.com>
2023-04-08 16:50:21 +0100

    31

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,28 +1,29 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  // ... other config ...
-
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true,
-    compress: true,
-    hot: true,
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:2000',
-        pathRewrite: { '^/api': '' },
-        secure: false,
-        changeOrigin: true,
-      },
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
     },
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-};
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './public/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            // apiUrl: 'http://localhost:4000'
+            apiUrl: '/api'
+        })
+    }
+}
