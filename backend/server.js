44aa3621393298aadba39982dda071150ab790d3
VincentWang1024 <v1ncent.wang1024@gmail.com>
2023-04-08 01:03:09 +0100

    cc

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
 var express = require("express");
 var app = express();
 
-const cors = require('cors');
-app.use(cors());
-
 const bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
